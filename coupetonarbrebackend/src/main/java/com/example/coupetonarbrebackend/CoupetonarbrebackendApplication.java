867f02ccc180ef842702d4899406af84e830fcfe
eb07 <EvanBond007@outlook.com>
2023-11-24 13:47:27 -0500

    Added backend files

---
+++ coupetonarbrebackend/src/main/java/com/example/coupetonarbrebackend/CoupetonarbrebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupetonarbrebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupetonarbrebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoupetonarbrebackendApplication.class, args);
+    }
+
+}
