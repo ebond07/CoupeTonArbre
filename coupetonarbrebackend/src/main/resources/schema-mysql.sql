c9c38e13cc537cfbd3b8ea9e2ac221174a7b8356
dazhuox <dazhuoxie1024@gmail.com>
2023-11-27 23:06:33 -0500

    Added schema-mysql.sql file for the schema of the database (tables)

---
+++ coupetonarbrebackend/src/main/resources/schema-mysql.sql
@@ -0,0 +1,12 @@
+USE `coupetonarbre-db`;
+
+-- to be modified
+create table if not exists clients (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    client_id VARCHAR(36),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    postal_code VARCHAR (9)
+);
+
