0840e11ace71a5e04d82370324f0b6858e2c75c6
dazhuox <dazhuoxie1024@gmail.com>
2023-11-28 15:10:53 -0500

    Gradle version 8.4, mysql version latest, removed postal code

--- coupetonarbrebackend/src/main/resources/schema-mysql.sql
+++ coupetonarbrebackend/src/main/resources/schema-mysql.sql
@@ -1,13 +1,11 @@
 USE `coupetonarbre-db`;
 
 -- to be modified
-create table if not exists clients (
+CREATE TABLE IF NOT EXISTS clients (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    client_id VARCHAR(36),
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(50),
-    address VARCHAR(50),
-    postal_code VARCHAR (9)
+    client_id VARCHAR(36) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    address VARCHAR(50) NOT NULL
 );
-
