6ae719a78ebace96be3c1f62767a759c488d94df
David Rallo <davidrallo28@gmail.com>
2023-11-30 18:26:09 -0500

    Fixed build error

--- coupetonarbrebackend/src/main/java/com/example/coupetonarbrebackend/CoupetonarbrebackendApplication.java
+++ coupetonarbrebackend/src/main/java/com/example/coupetonarbrebackend/CoupetonarbrebackendApplication.java
@@ -2,6 +2,7 @@ package com.example.coupetonarbrebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 ;
