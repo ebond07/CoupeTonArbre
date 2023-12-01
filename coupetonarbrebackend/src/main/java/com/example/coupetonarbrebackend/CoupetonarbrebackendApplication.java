427cf7e5f016e22d974629c1bcb61470704507df
David Rallo <davidrallo28@gmail.com>
2023-11-30 19:42:29 -0500

    Fixed main

--- coupetonarbrebackend/src/main/java/com/example/coupetonarbrebackend/CoupetonarbrebackendApplication.java
+++ coupetonarbrebackend/src/main/java/com/example/coupetonarbrebackend/CoupetonarbrebackendApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class CoupetonarbrebackendApplication {
