8b0e8107ed4a052b88b00e2c03a1a1a79f977de5
dazhuox <dazhuoxie1024@gmail.com>
2023-12-02 16:32:25 -0500

    Connect App.js with HomePage component
    Created HomePage.jsx and connected it successfully with App.js. npm start now displays HomePage.jsx component

---
+++ coupetonarbrefrontend/src/components/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <div id='nav-container'>
+        <nav>
+          <ul>
+            <li><a href='HomePage.jsx'>Home</a></li>
+            <li><a href=''>Home</a></li>
+            <li><a href='HomePage.jsx'>Home</a></li>
+            <li><a href='HomePage.jsx'>Home</a></li>
+          </ul>
+        </nav>
+      </div>
+      
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the content of the home page.</p>
+    </div>
+  );
+}
+
+export default HomePage;
