c9098878d1cfc3c8cc29e9a49ef3a93749f0153d
Matthewtan9 <matthew999099@gmail.com>
2023-12-15 12:38:18 -0500

    Front end and Front End Testing

--- coupetonarbrefrontend/src/components/HomePage.jsx
+++ coupetonarbrefrontend/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 
+
 function HomePage() {
   return (
     <div>
@@ -10,7 +11,10 @@ function HomePage() {
       
       <h2>Welcome to the Home Page</h2>
       <p>This is the content of the home page.</p>
+      
+
     </div>
+    
   );
 }
 
