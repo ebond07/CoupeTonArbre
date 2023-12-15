f0c8cc72912aa0aa8fff18d3c7e64ff09fbcaecd
David Rallo <davidrallo28@gmail.com>
2023-12-15 16:26:55 -0500

    Rebased getById and Delete

--- coupetonarbrebackend/src/main/java/com/example/coupetonarbrebackend/User/BusinessLayer/ClientService.java
+++ coupetonarbrebackend/src/main/java/com/example/coupetonarbrebackend/User/BusinessLayer/ClientService.java
@@ -1,5 +1,6 @@
 package com.example.coupetonarbrebackend.User.BusinessLayer;
 
+import com.example.coupetonarbrebackend.User.PresentationLayer.ClientRequestDTO;
 import com.example.coupetonarbrebackend.User.PresentationLayer.ClientResponseDTO;
 
 
@@ -8,6 +9,8 @@ import java.util.List;
 public interface ClientService {
 
     List<ClientResponseDTO> getAllClients();
+
+    ClientResponseDTO updateClient(ClientRequestDTO clientRequestDTO, String clientId);
     ClientResponseDTO getClientById(String id);
 
     void deleteClientByClientId(String clientId);
